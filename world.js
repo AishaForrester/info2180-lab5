5c894d928ec5ab7342392218e3cfba3eaaed3563
Aisha Forrester <AishaForrester@gmail.com>
2023-11-20 21:21:46 -0500

    Final Product

--- world.js
+++ world.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var lookupB = document.getElementById('lookup');
-    var uInput =  document.getElementById('country');
-    var resDiv =  document.getElementById('result');
+    var lookupB = document.getElementById('lookup'); //first button
+    var nlookupB = document.getElementById('nlookup'); //second button
+    var uInput =  document.getElementById('country'); //user input
+    var resDiv =  document.getElementById('result'); //result div
 
     lookupB.addEventListener('click', function(){
         var newSearch = uInput.value //grabbing user input
 
-        //here we will open an ajax connection to sort of ask the php for data from database
+        //here we will open an ajax connection to sort of ask the php for country data from database
 
         fetch('world.php?country=' + encodeURIComponent(newSearch))
             .then(response => response.text())
@@ -18,6 +19,23 @@ document.addEventListener('DOMContentLoaded', function(){
             })
 
     }); //end of event listener for when button is clicked
+
+    nlookupB.addEventListener('click', function(){  //add eventlistener to second button
+        var newSearch = uInput.value  //again, grabbing user input
+
+        //here we will open an ajax connection to sort of ask the php for city data from database
+
+        fetch('world.php?country=' + encodeURIComponent(newSearch) + '&lookup=cities')
+            .then(response => response.text())
+            .then(data =>{  //here we got new data for request
+                resDiv.innerHTML = data  //we appropriately copy our data into the result div
+            })
+            .catch(error => {  //error handling in case we don't get this data or something happened
+                console.error("Encountered error fetching your data:", error);
+            })
+
+    });  //end of second eventlistener
+
 
 
 
--- world.php
+++ world.php
@@ -11,11 +11,23 @@ try {
     // Check if the 'country' parameter is set in the GET request
     $country = isset($_GET['country']) ? $_GET['country'] : '';
 
-    // Use a prepared statement to prevent SQL injection
+    // Check if the 'lookup' parameter is set to 'cities'
+    $lookupType = isset($_GET['lookup']) ? $_GET['lookup'] : '';
+
     if (!empty($country)) {
-        // Partial match using LIKE when 'country' is not empty
         $searchCountry = '%' . $country . '%';
-        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+
+        if ($lookupType === 'cities') {
+            // Query for our cities
+            $stmt = $conn->prepare("SELECT cities.name AS city_name, cities.district, cities.population, countries.name AS country_name
+                                   FROM cities
+                                   JOIN countries ON cities.country_code = countries.code
+                                   WHERE countries.name LIKE :country");
+        } else {
+            // Query for our countries
+            $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        }
+
         $stmt->bindParam(':country', $searchCountry, PDO::PARAM_STR);
     } else {
         // Fetch all countries when 'country' is empty
@@ -26,6 +38,7 @@ try {
 
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    // Output HTML table
     echo '<style>';
     echo 'table {border-collapse: collapse; width: 100%;}';
     echo 'th, td {border: 1px solid #dddddd; text-align: left; padding: 8px;}';
@@ -33,15 +46,30 @@ try {
     echo '</style>';
 
     echo '<table>';
-    echo '<thead><tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr></thead>';
+    if ($lookupType === 'cities') {
+        // Table header for cities
+        echo '<thead><tr><th>City Name</th><th>Country Name</th><th>District</th><th>Population</th></tr></thead>';
+    } else {
+        // Table header for countries
+        echo '<thead><tr><th>Country Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr></thead>';
+    }
     echo '<tbody>';
 
     foreach ($results as $row) {
         echo '<tr>';
-        echo '<td>' . $row['name'] . '</td>';
-        echo '<td>' . $row['continent'] . '</td>';
-        echo '<td>' . $row['independence_year'] . '</td>';
-        echo '<td>' . $row['head_of_state'] . '</td>';
+        if ($lookupType === 'cities') {
+            // Table row for cities
+            echo '<td>' . $row['city_name'] . '</td>';
+            echo '<td>' . $row['country_name'] . '</td>';
+            echo '<td>' . $row['district'] . '</td>';
+            echo '<td>' . $row['population'] . '</td>';
+        } else {
+            // Table row for countries
+            echo '<td>' . $row['name'] . '</td>';
+            echo '<td>' . $row['continent'] . '</td>';
+            echo '<td>' . $row['independence_year'] . '</td>';
+            echo '<td>' . $row['head_of_state'] . '</td>';
+        }
         echo '</tr>';
     }
 
